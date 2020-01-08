8041f61b9183ae2d2c9bb9dcaf5b634fdb14268d
ail2vu <niancode@gmail.com>
2020-01-09 02:20:08 +0800

    PS-2019.3.1 <ail2vu@imac.lan Create markdown.xml, PHP Interface Doc Comment.php, PHP Class.php, ide.general.xml, Sublime Text _macOS_ copy.xml, project.default.xml, PHP Trait Doc Comment.php, PHP Class Doc Comment.php, terminal.xml, Default.xml, file.template.settings.xml, ui.lnf.xml, php.xml, colors.scheme.xml, notifications.xml, databaseDrivers.xml, templates.xml, PHP Function Doc Comment.php, PHP.xml, PHP File Header.php, laf.xml, PHP Property Doc Comment.php, proxy.settings.xml, vcs.xml, keymap.xml, editor.xml, debugger.xml, PHP File.php

---
+++ fileTemplates/includes/PHP File Header.php
@@ -0,0 +1,8 @@
+/**
+ * @Author: Ali2vu <751815097@qq.com>
+ * @Date: ${YEAR}-${MONTH}-${DAY} ${HOUR}:${MINUTE}:${SECOND}
+ * @LastEditors: Ali2vu
+ * @LastEditTime: ${YEAR}-${MONTH}-${DAY} ${HOUR}:${MINUTE}:${SECOND}
+ */
+ 
+declare(strict_types=1);
---
+++ fileTemplates/includes/PHP Function Doc Comment.php
@@ -0,0 +1,8 @@
+/**
+* ${CARET}
+* @author Ali2vu <751815097@qq.com>
+${PARAM_DOC}
+#if (${TYPE_HINT} != "void") * @return ${TYPE_HINT}
+#end
+${THROWS_DOC}
+*/
